dcf24dc1cf5b6f64d07d9a5452b0c5e08bd6d4b0
bjtromp-GH <bjtromp79@gmail.com>
2026-06-17 19:28:06 +0200

    refactor(ui): reorder components in MobileContainer
    Move AestheticFidelityCards below the LifePhasesBar and DecadeGrid to improve visual flow on slide 2.

--- src/components/MobileContainer.tsx
+++ src/components/MobileContainer.tsx
@@ -211,13 +211,6 @@ export default function MobileContainer({
 
               {slideIndex === 2 && (
                 <div className="space-y-4 pb-4">
-                  {/* High Fidelity Stacked Cards */}
-                  <AestheticFidelityCards
-                    inputs={inputs}
-                    projectedLifeExpectancy={projectedLifeExpectancy}
-                    apiSource={apiSource}
-                  />
-
                   {/* Tiny Life phases bar */}
                   <div className="bg-white p-3 border border-[#EAEAEA] rounded-md">
                     <LifePhasesBar
@@ -234,6 +227,13 @@ export default function MobileContainer({
                       projectedLifeExpectancy={projectedLifeExpectancy}
                     />
                   </div>
+
+                  {/* High Fidelity Stacked Cards */}
+                  <AestheticFidelityCards
+                    inputs={inputs}
+                    projectedLifeExpectancy={projectedLifeExpectancy}
+                    apiSource={apiSource}
+                  />
                 </div>
               )}
             </motion.div>
